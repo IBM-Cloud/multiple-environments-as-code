0294611c49735b8063739d5542b44feda61a1b19
Henrik Loeser <data-henrik@users.noreply.github.com>
2018-06-05 15:10:47 +0200

    Change CLI command from bx to ibmcloud

--- iam/development/add-operator.sh
+++ iam/development/add-operator.sh
@@ -4,22 +4,22 @@ USER=$1
 GROUP="Example-Operator-Role"
 
 # Check if the group exist
-if bx iam access-group $GROUP >/dev/null; then
+if ibmcloud iam access-group $GROUP >/dev/null; then
   echo "Role already exists"
 else
   # Create the access group for the role if the group does not exist
-  bx iam access-group-create $GROUP --description "used by the multiple-environments-as-code tutorial"
+  ibmcloud iam access-group-create $GROUP --description "used by the multiple-environments-as-code tutorial"
 
   # Set the permissions for this group
   # Resource Group: Viewer
-  bx iam access-group-policy-create $GROUP --roles Viewer --resource-type resource-group --resource "default"
+  ibmcloud iam access-group-policy-create $GROUP --roles Viewer --resource-type resource-group --resource "default"
 
   # Platform Access Roles in the Resource Group: Operator, Viewer
-  bx iam access-group-policy-create $GROUP --roles Operator,Viewer --resource-group-name "default"
+  ibmcloud iam access-group-policy-create $GROUP --roles Operator,Viewer --resource-group-name "default"
 
   # Monitoring: Administrator, Editor, Viewer
-  bx iam access-group-policy-create $GROUP --roles Administrator,Editor,Viewer --service-name monitoring
+  ibmcloud iam access-group-policy-create $GROUP --roles Administrator,Editor,Viewer --service-name monitoring
 fi
 
 # Add the user to the group
-bx iam access-group-user-add $GROUP $USER
+ibmcloud iam access-group-user-add $GROUP $USER
